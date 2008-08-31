f0e351260e0ebfee4b0665a6948bbfb706821884
Jelmer Vernooij <jelmer@samba.org>
2008-08-31 21:05:38 +0200

    Fix module name.

--- tests/mapping3/__init__.py
+++ tests/mapping3/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sha1
+import sha
 
 from bzrlib.tests import TestCase
 
