5192b6c5856dc17289171ddc76b73eb491e28ba2
Jelmer Vernooij <jelmer@samba.org>
2008-10-02 23:17:56 +0200

    Fix exception module name.

--- subvertpy/util.c
+++ subvertpy/util.c
@@ -69,7 +69,7 @@ void PyErr_SetSubversionException(svn_error_t *error)
 		return;
 	}
 
-	coremod = PyImport_ImportModule("bzrlib.plugins.svn.subvertpy");
+	coremod = PyImport_ImportModule("subvertpy");
 
 	if (coremod == NULL) {
 		return;
