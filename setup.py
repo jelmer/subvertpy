d95d96a1d53c7d7a5e2f7aab1a50ef66d0e62342
Jelmer Vernooij <jelmer@samba.org>
2009-10-20 21:53:14 +0200

    release 0.7.1.

--- setup.py
+++ setup.py
@@ -297,7 +297,7 @@ def subvertpy_modules(basemodule):
         ]
 
 
-subvertpy_version = (0, 7, 0)
+subvertpy_version = (0, 7, 1)
 subvertpy_version_string = ".".join(map(str, subvertpy_version))
 
 
