72188cd85191bc41381f7097bfc8eae3d6c3533e
Jelmer Vernooij <jelmer@samba.org>
2011-03-03 18:29:35 +0100

    Fix compilation on OS X.

--- subvertpy/editor.h
+++ subvertpy/editor.h
@@ -45,6 +45,6 @@ extern void initeditor(void);
 
 svn_error_t *py_txdelta_window_handler(svn_txdelta_window_t *window, void *baton);
 
-const svn_delta_editor_t py_editor;
+extern const svn_delta_editor_t py_editor;
 
 #endif /* _BZR_SVN_EDITOR_H_ */
