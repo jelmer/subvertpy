3226490538b4a443a104877683c22736dbf7620e
Yonggang Luo <luoyonggang@gmail.com>
2012-10-19 00:04:38 +0800

    Use six.moves.xrange.

--- subvertpy/delta.py
+++ subvertpy/delta.py
@@ -100,7 +100,7 @@ def txdelta_apply_ops(src_ops, ops, new_data, sview):
             # Copy from source area.
             tview += sview[offset:offset+length]
         elif action == TXDELTA_TARGET:
-            for i in xrange(length):
+            for i in moves.xrange(length):
                 tview += tview[offset+i]
         elif action == TXDELTA_NEW:
             tview += new_data[offset:offset+length]
