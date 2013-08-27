71dd819244fc07f1c52b0be51f2a23c862150e60
Martin Panter <vadmium  gmailcom>
2013-08-27 14:28:31 +0000

    Use new print() function syntax, with help from “2to3”

--- examples/ra_replay.py
+++ examples/ra_replay.py
@@ -10,7 +10,7 @@ conn = RemoteAccess("svn://svn.gnome.org/svn/gnome-specimen/trunk",
 class MyFileEditor:
     
     def change_prop(self, key, value):
-        print "Change prop: %s -> %r" % (key, value)
+        print("Change prop: %s -> %r" % (key, value))
 
     def apply_textdelta(self, base_checksum):
         # This should return a function that can receive delta windows
@@ -24,26 +24,26 @@ class MyFileEditor:
 class MyDirEditor:
 
     def open_directory(self, *args):
-        print "Open dir: %s (base revnum: %r)" % args
+        print("Open dir: %s (base revnum: %r)" % args)
         return MyDirEditor()
 
     def add_directory(self, path, copyfrom_path=None, copyfrom_rev=-1):
-        print "Add dir: %s (from %r:%r)" % (path, copyfrom_path, copyfrom_rev)
+        print("Add dir: %s (from %r:%r)" % (path, copyfrom_path, copyfrom_rev))
         return MyDirEditor()
 
     def open_file(self, *args):
-        print "Open file: %s (base revnum: %r)" % args
+        print("Open file: %s (base revnum: %r)" % args)
         return MyFileEditor()
 
     def add_file(self, path, copyfrom_path=None, copyfrom_rev=-1):
-        print "Add file: %s (from %r:%r)" % (path, copyfrom_path, copyfrom_rev)
+        print("Add file: %s (from %r:%r)" % (path, copyfrom_path, copyfrom_rev))
         return MyFileEditor()
 
     def change_prop(self, key, value):
-        print "Change prop %s -> %r" % (key, value)
+        print("Change prop %s -> %r" % (key, value))
 
     def delete_entry(self, path, revision):
-        print "Delete: %s" % path
+        print("Delete: %s" % path)
 
     def close(self):
         pass
@@ -52,16 +52,16 @@ class MyDirEditor:
 class MyEditor:
 
     def set_target_revision(self, revnum):
-        print "Target revision: %d" % revnum
+        print("Target revision: %d" % revnum)
 
     def abort(self):
-        print "Aborted"
+        print("Aborted")
 
     def close(self):
-        print "Closed"
+        print("Closed")
 
     def open_root(self, base_revnum):
-        print "/"
+        print("/")
         return MyDirEditor()
 
 
