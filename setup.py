ae39932d4a4174be4668fc7ad4cc4353a3a063aa
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-07-19 23:42:02 +0100

    Drop support for Python < 3.5.

--- setup.py
+++ setup.py
@@ -299,7 +299,6 @@ and Mac OS X).
               'Development Status :: 4 - Beta',
               'License :: OSI Approved :: GNU General Public '
               'License v2 or later (GPLv2+)',
-              'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
               'Programming Language :: Python :: 3.6',
